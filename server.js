46fd6e6192d7360bd698ae406bd83012530ee0ca
lucas.valenzuela <lucas.valenzuela@squadra.com.br>
2020-08-01 18:47:52 -0300

    L#67

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
@@ -10,14 +11,17 @@ app.use(express.json({ extended: false }));
 
 app.use(cors());
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the ContactKeeper API...' })
-);
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
